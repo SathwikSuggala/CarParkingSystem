5c1f179cbbe193b41d068d4cd0f07dc98c48259e
SathwikSuggala <125736441+SathwikSuggala@users.noreply.github.com>
2023-03-05 19:58:57 +0530

    Add files via upload

---
+++ ReadFile2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReadFile2 {
+
+    static ArrayList<NewRegistration> list1 = new ArrayList<>();
+    static NewRegistration line1;
+
+    public static void readFromFile()
+    {
+        list1.clear();
+        File fr = new File("cars parked.txt");
+        try {
+            Scanner sc = new Scanner(fr);
+            while (sc.hasNextLine()) {
+                String name = sc.nextLine();
+                String regNo = sc.nextLine();
+                LocalDateTime o = LocalDateTime.parse(sc.nextLine());
+
+                line1 = new NewRegistration(name, regNo, o);
+                list1.add(line1);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
